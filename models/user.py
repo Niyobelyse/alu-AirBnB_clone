ff20c44358f435fcb068dcea3ac67963bb28604d
Niyonsenga-Belyse <b.niyonseng@alustudent.com>
2024-09-13 14:45:32 +0200

    correct output for question8

--- models/user.py
+++ models/user.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-
-"""This file defines the UserModel class
-It inherits from the BaseModel
 """
-
+This module defines the User class which inherits from BaseModel.
+"""
 
 from models.base_model import BaseModel
 
-
 class User(BaseModel):
-    """The User Model"""
+    """
+    The User class represents a user in the AirBnB clone.
+    It inherits from BaseModel and has attributes specific to a user.
+    """
 
-    # Attributes
     email = ""
     password = ""
     first_name = ""
     last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize a new User instance.
+        """
+        super().__init__(*args, **kwargs)
